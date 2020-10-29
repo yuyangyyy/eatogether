511090fba71ed2f5aae6244732dc0e777fdd1fa3
AdelKaced <adel.kaced@gmail.com>
2020-10-29 12:36:04 +0100

    working recipe

--- src/component/RecipeCard.js
+++ src/component/RecipeCard.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import './RecipeCard.css';
+import React from "react";
+import "./RecipeCard.css";
 
-const key = '103f7895318eab82a99d13cab66cde64';
-const id = '874300a6';
-const foods = ['fish', 'chicken', 'vegetables', 'potato', 'lasagnes'];
+const key = "103f7895318eab82a99d13cab66cde64";
+const id = "874300a6";
+const foods = ["fish", "chicken", "vegetables", "potato", "mexican"];
 
 class RecipeCard extends React.Component {
   state = {
-    food: []
-  }
-  /**
-  {
+    food: [
+      {
         image: 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search-v2_297x176.jpg',
         name: 'image test'
       },
@@ -27,7 +25,8 @@ class RecipeCard extends React.Component {
         name: 'image test'
       }
     ]
-  */
+  }
+
 
   getRecipe = () => {
     foods.forEach((para) => {
@@ -36,37 +35,39 @@ class RecipeCard extends React.Component {
       )
         .then((response) => response.json())
         .then((data) => {
+          // const random = Math.floor(Math.random() * 10);
           this.setState({
             food: [
               ...this.state.food,
               {
                 image: data.hits[0].recipe.image,
-                name: data.hits[0].recipe.label
-              }
-            ]
+                name: data.hits[0].recipe.label,
+              },
+            ],
           });
         });
     });
-  }
+  };
 
-  componentDidMount () {
+  /** componentDidMount() {
     this.getRecipe();
-  }
+  } */
 
-  render () {
+  render() {
     console.log(this.state.food);
     return (
-      <div className='recipe-card '>
-        {this.state.food.length > 0 && this.state.food.map((para, index) => {
-          console.log('truc');
-          return (
-        <div className='item-card' key={index} >
-            <p>{para.name}</p>
-            <img src={para.image} alt={para.name} />
-        </div>
-
-          );
-        })}
+      <div className="recipe-card ">
+        {this.state.food.length > 0 &&
+          this.state.food.map((para, index) => {
+            console.log("truc");
+            return (
+              <div className="item-card" key={index}>
+                <h1>{para.name}</h1>
+                <img src={para.image} alt={para.name} />
+                <p>{foods[index].toUpperCase()}</p>
+              </div>
+            );
+          })}
       </div>
     );
   }
