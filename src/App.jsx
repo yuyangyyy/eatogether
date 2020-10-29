826db75148570734f465c1d0ebe7f7f484c63549
Youssouf <youssouf.adenomar@gmail.com>
2020-10-29 10:48:05 +0100

    host-information

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
+import React from 'react';
 import './App.css';
+import Display from './Components/Display';
 
-function App() {
+import Display2 from './Components/Display2';
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React starter template</h1>
-        <p>Since React 17, you don&#39;t need this anymore:</p>
-        <pre>
-          <code>import React from &#39;react&#39;;</code>
-        </pre>
-      </header>
+    <div>
+      <Display/>
+      <Display2/>
     </div>
   );
 }
---
+++ src/Components/Display2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Display2.css';
+
+function Display2 () {
+  return (
+        <section className = "n ">
+          <div> <h2 className = "Proposer"> Je Propose</h2>
+           <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus turpis eu molestie lacinia. Pellentesque eget rhoncus lacus, vitae sodales turpis. Maecenas finibus, urna id auctor ultrices, ante neque mattis erat, a dapibus arcu augue a mi. Nulla viverra leo mi, vitae laoreet purus pellentesque nec. Aenean malesuada lacus lacus, quis molestie dolor luctus ac. Sed placerat urna ut metus finibus tristique. Aliquam vehicula risus massa. Ut tempor pretium tincidunt. Morbi malesuada nulla quis velit posuere imperdiet. Nunc laoreet nisl libero, nec fringilla ligula sollicitudin a.</p>
+           </div>
+
+            <div>
+
+          <h2 className = "Manger"> Je Mange</h2>
+           <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus turpis eu molestie lacinia. Pellentesque eget rhoncus lacus, vitae sodales turpis. Maecenas finibus, urna id auctor ultrices, ante neque mattis erat, a dapibus arcu augue a mi. Nulla viverra leo mi, vitae laoreet purus pellentesque nec. Aenean malesuada lacus lacus, quis molestie dolor luctus ac. Sed placerat urna ut metus finibus tristique. Aliquam vehicula risus massa. Ut tempor pretium tincidunt. Morbi malesuada nulla quis velit posuere imperdiet. Nunc laoreet nisl libero, nec fringilla ligula sollicitudin a.</p>
+         </div>
+           </section>);
+}
+export default Display2;
