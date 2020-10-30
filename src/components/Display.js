0dac5b2e971c7028a44b973f753838b5484571b8
Muriel Jakazi <murielnjakazi@gmail.com>
2020-10-30 18:03:05 +0100

    combined parts of home page

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,27 +4,34 @@ import './Display.css';
 function Display () {
   return (
     <section className="display-container">      
-      <img className="display-image" src="https://greenkitchenstories.com/wp-content/uploads/2016/11/gks_hot_chocolate_3.jpg" alt="cuisine" />
           <div className="display-evening">
-            <h1>Plan Your Night</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus turpis eu molestie lacinia. Pellentesque eget rhoncus lacus, vitae sodales turpis. Maecenas finibus, urna id auctor ultrices, ante neque mattis erat, a dapibus arcu augue a mi. Nulla viverra leo mi, vitae laoreet purus pellentesque nec. Aenean malesuada lacus lacus, quis molestie dolor luctus ac.
-            </p>
-            <input id="button" className="hostButton" type="button" value="Suggest meal"></input>
-        </div>
-      
-
-      <div className = "siteDetails">
-      <div className = "Proposer">
-          <h2>Host</h2>
-          <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus turpis eu molestie lacinia. Pellentesque eget rhoncus lacus, vitae sodales turpis. Maecenas finibus, urna id auctor ultrices, ante neque mattis erat, a dapibus arcu augue a mi. Nulla viverra leo mi, vitae laoreet purus pellentesque nec. Aenean malesuada lacus lacus, quis molestie dolor luctus ac. Sed placerat urna </p>
-      </div>
-
-      <div className = "Manger">
-          <h2>Guest</h2>
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus turpis eu molestie lacinia. Pellentesque eget rhoncus lacus, vitae sodales turpis. Maecenas finibus, urna id auctor ultrices, ante neque mattis erat, a dapibus arcu augue a mi. Nulla viverra leo mi, vitae laoreet purus pellentesque nec. Aenean malesuada lacus lacus, quis molestie dolor luctus ac. Sed placerat urna </p>
-    </div>
-      </div>
+             <div className="display-image">
+              <img src="https://greenkitchenstories.com/wp-content/uploads/2016/11/gks_hot_chocolate_3.jpg" alt="cuisine" />
+             </div>            
+              <div className="display-host-planning">
+                <div className="display-text-info">
+                  <h2>Plan Your Night</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus turpis eu molestie lacinia. Pellentesque eget rhoncus lacus, vitae sodales turpis. Maecenas finibus, urna id auctor ultrices, ante neque mattis erat, a dapibus arcu augue a mi. Nulla viverra leo mi, vitae laoreet purus pellentesque nec. Aenean malesuad.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing e
+                  </p>
+                </div>
+                <input type="button" value="Suggest meal"/>
+              </div>                            
+          </div> 
+          <hr className="dislay-hr-top"/>          
+          <div className = "display-details">
+            <div className = "display-host">
+                <h3>Host</h3>
+                <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus turpis eu molestie lacinia. Pellentesque eget rhoncus lacus, vitae sodales turpis. Maecenas finibus, urna id auctor ultrices, ante neque mattis erat, a dapibus arcu augue a mi. Nulla viverra leo mi, vitae laoreet purus pellentesque nec. Aenean malesuada lacus lacus, quis molestie dolor luctus ac. Sed placerat urna </p>
+            </div>
+            <div className = "display-guest">
+                <h3>Guest</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus turpis eu molestie lacinia. Pellentesque eget rhoncus lacus, vitae sodales turpis. Maecenas finibus, urna id auctor ultrices, ante neque mattis erat, a dapibus arcu augue a mi. Nulla viverra leo mi, vitae laoreet purus pellentesque nec. Aenean malesuada lacus lacus, quis molestie dolor luctus ac. Sed placerat urna </p>
+            </div>
+            
+          </div>
+          <hr className="dislay-hr-bottom"/>
     </section>
-     );
+  );
 }
 export default Display;
