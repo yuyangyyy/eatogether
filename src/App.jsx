22ee52ac10f34d9534312a11016a575d7ec61be3
Romain heloise <romheloise@gmail.com>
2020-11-05 17:42:06 +0100

    change position

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Host from "./components/Host";
 
-import Home from "./components/Home"
 import Navigation from "./components/Navigation"
+import Home from "./components/Home"
+import Host from "./components/Host";
 
 import "./App.css";
 
@@ -18,9 +18,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Host />
         <Navigation getName={this.getName} />
         <Home />        
+        <Host />
       </div>
     );
   }
