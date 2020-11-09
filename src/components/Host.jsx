94e87049215f29d1ff4332843c328cba2ab3f3aa
Romain heloise <romheloise@gmail.com>
2020-11-09 20:46:51 +0100

    use props chidlren

--- src/components/Host.jsx
+++ src/components/Host.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import SearchRecipe from "./SearchRecipe/SearchRecipe";
+import SearchRecipeLeft from "./SearchRecipe/SearchRecipeLeft";
+import SearchRecipeRight from "./SearchRecipe/SearchRecipeRight";
 
 class Host extends React.Component {
   state = {
@@ -8,13 +10,25 @@ class Host extends React.Component {
   };
 
   getRecipeResult = (result) => {
+    console.log(result)
     this.setState({ recipeResult: result });
   };
+
+  key = "1a44f438887545cc993645317074f023";
 
   render() {
     return (
       <div>
-        <SearchRecipe recipeResult={this.getRecipeResult} />
+        <SearchRecipe>
+          <SearchRecipeLeft
+            recipeResult={this.getRecipeResult}
+            keyApi={this.key}
+          />
+          <SearchRecipeRight
+            recipeResult={this.getRecipeResult}
+            keyApi={this.key}
+          />
+        </SearchRecipe>
       </div>
     );
   }
--- src/components/SearchRecipe/SearchRecipe.jsx
+++ src/components/SearchRecipe/SearchRecipe.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 
-import SearchRecipeLeft from "./SearchRecipeLeft";
-import SearchRecipeRight from "./SearchRecipeRight";
-
 import "./scss/SearchRecipe.scss";
 
 const SearchRecipe = (props) => {
-  const key = "1734d5ded2604d979f0886c3d7182230";
-
   return (
     <div className="search-recipe-wrapper">
       <h2 className="search-recipe-wrapper-title">Search for a Recipe</h2>
 
-      <div className="search-recipe-blocs">
-        <SearchRecipeLeft recipeResult={props.recipeResult} keyApi={key} />
-        <SearchRecipeRight recipeResult={props.recipeResult} keyApi={key} />
-      </div>
+      <div className="search-recipe-blocs">{props.children}</div>
     </div>
   );
 };
