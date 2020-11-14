0b5013f02d546d643e8d6c7e5c31bffe1dc96eeb
AdelKaced <adel.kaced@gmail.com>
2020-11-14 09:34:56 +0100

    add css guest result

--- src/components/GuestResult.js
+++ src/components/GuestResult.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { UserContext } from '../App';
+
+
+import './GuestResult.css';
 
 const GuestResult = () => {
   console.log("hi");
-  return <div>Test</div>;
+  const userInfo = useContext(UserContext);
+  console.log(userInfo);
+
+  return (
+    <div>test</div>
+    // <div className="guest-container">
+    //   <div className="guest-result">
+    //     <p className="guest-invit">Your Invitation</p>
+    //     <img
+    //       className="guest-img"
+    //       src={selectRecipe.image}
+    //       alt={selectRecipe.name}
+    //     />
+
+    //     <div className="guest-info">
+    //       <p>
+    //         Name :{' '}
+    //         {userInfo.userName.firstName + ' ' + userInfo.userName.lastName}
+    //       </p>
+    //       <p>Email:{userInfo.userName.email}</p>
+    //       <p>Date:{}</p>
+    //       <p>City:{}</p>
+    //     </div>
+    //   </div>
+    // </div>
+  )
 };
 
 export default GuestResult;
